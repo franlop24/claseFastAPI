2cccf072a1f9d7daa4b718ea9c0b2f8c1ead6a41
Francisco Lopez <franlopbri@gmail.com>
2022-03-01 15:40:24 -0600

    Validaciones Path Parameters

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from typing import Optional
 # Import de Pydantic
 
 # Imports de fastAPI
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Path, Query
 
 app = FastAPI()
 
@@ -24,23 +24,34 @@ def alumnos():
         }]
     return alumnos
 
-# PATH PARAMETERS
+# PATH PARAMETERS son Obligatorios Required
 @app.get("/alumnos/{id_alumno}")
-def alumno_por_id(id_alumno: int):
+def alumno_por_id(
+                    id_alumno: int = Path(
+                        ...,
+                        gt=0,
+                        title="ID del alumno",
+                        description="Busca algun alumno por ID"
+                    )
+                ):
     return {id_alumno: "Este alumno sí existe"}
 
-# QUERY PARAMETERS
+# QUERY PARAMETERS con Opcionales
 @app.get("/alumno/query")
 def query_alumnos(
                     grupo: Optional[str] = Query(
                         None,
                         min_length=1,
-                        max_length=1
+                        max_length=1,
+                        title="Grupo",
+                        description="Regresa los alumnos de un grupo"
                     ), 
                     cuatri: Optional[int] = Query(
                         None,
                         ge=1,
-                        lt=12
+                        lt=12,
+                        title="Cuatrimestre",
+                        description="Cuatrimestre del grupo"
                     )
                 ):
     return {
